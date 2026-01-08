6bf198057988de912914e0e7bb2e8cc0cc4b4176
tezalcorp <thomas@tezalcorp.com>
2026-01-08 11:21:57 -0600

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from '@/App.jsx'
-import '@/index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "@/App.jsx";
+import "@/index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <QueryClientProvider client={queryClient}>
     <App />
-) 
+  </QueryClientProvider>
+);
